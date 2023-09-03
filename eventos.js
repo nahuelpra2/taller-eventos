d890383eae080e3deeaf8b35320b1d5cdf4bc5b8
Felpo-J <nahuelprado16@gmail.com>
2023-09-03 20:14:05 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -1,9 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Selecciono el div
     const miDiv = document.getElementById("midiv"); 
+    const btn = document.querySelector("button")
 
     // Agrego el manejador de eventos para el clic en el div:
     miDiv.addEventListener("click", function() {
         alert("Hola! Soy el div");
+        
+    });
+
+    btn.addEventListener("click", function(e) {
+        e.stopPropagation();
+        
     });
 });
